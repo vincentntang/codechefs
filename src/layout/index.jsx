94b8224350f0030dc3b7e159d89c9f84223471cb
vincenttang <10290348+vincentntang@users.noreply.github.com>
2020-04-27 09:11:50 -0400

    gatsby-advanced-starter forked-390

---
+++ src/layout/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Helmet from "react-helmet";
+import config from "../../data/SiteConfig";
+import "./index.css";
+
+export default class MainLayout extends React.Component {
+  render() {
+    const { children } = this.props;
+    return (
+      <div className="layout-container">
+        <Helmet>
+          <meta name="description" content={config.siteDescription} />
+          <html lang="en" />
+        </Helmet>
+        {children}
+      </div>
+    );
+  }
+}
