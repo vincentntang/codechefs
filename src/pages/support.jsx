dfa8e2f72656ccafbdc7d2f74083a6732ac76489
vincenttang <10290348+vincentntang@users.noreply.github.com>
2020-07-31 10:47:33 -0400

    add a support page

--- src/pages/support.jsx
+++ src/pages/support.jsx
@@ -8,6 +8,12 @@ const AboutPage = () => {
     <Layout>
       <div className="about-container">
         <Helmet title={`About | ${config.siteTitle}`} />
+        <div className="cc-card">
+          <div className="cc-padding cc-intro">
+            <p className="my-0"><span>🎤 </span>Code chefs is a podcast for developers seeking to learn more about webdevelopment. Whether your a fresh out of bootcamp graduate, or a fullstack veteran,
+              we do topics ranging for all levels. As developers who love learning, we hope you enjoy the content here</p>
+          </div>
+        </div>
       </div>
     </Layout>
   );
