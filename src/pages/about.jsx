377fc07aeaa1fb7e55cba30563b6acf5ec8140c8
vincenttang <10290348+vincentntang@users.noreply.github.com>
2020-10-08 01:01:51 -0400

    about and support content

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -15,8 +15,13 @@ const AboutPage = () => {
         <Helmet title={`About | ${config.siteTitle}`} />
         <div className="cc-header cc-card">
           <div className="cc-padding cc-intro">
-            <p className="my-0"><span>🎤 </span>Code chefs is a podcast for developers seeking to learn more about webdevelopment. Whether your a fresh out of bootcamp graduate, or a fullstack veteran,
-              we do topics ranging for all levels. As developers who love learning, we hope you enjoy the content here</p>
+            <p className="my-0"><span>🎤 </span>Code chefs is a podcast for the web developer who wants to level up! We do talks for all skill levels on:</p>
+            <ul>
+              <li>Javascript (React, Vue, Angular)</li>
+              <li>Backend + DevOps</li>
+              <li>CSS + Webdesign</li>
+              <li>Career Development</li>
+            </ul>
           </div>
           <div className="cc-portraits">
             <div className="cc-padding cc-portrait">
@@ -37,7 +42,7 @@ const AboutPage = () => {
                     <img src={socialGlobe} alt="social globe"/>
                   </a>
                 </div>
-                <div className="description">👨‍🍳 Vincent Tang is a fullstack developer from Orlando, FL. Formerly a kitchen designer. Loves the outdoors and trying new things</div>
+                <div className="description">👨‍🍳 Vincent Tang is a fullstack developer residing in Tampa, FL. Formerly a kitchen designer. Loves the outdoors and trying new things</div>
                
               </div>
             </div>
@@ -57,7 +62,7 @@ const AboutPage = () => {
                     <img src={socialGlobe} alt="social globe"/>
                   </a>
                 </div>
-                <div className="description">👨‍🍳 German Gamboa is a fullstack developer from Orlando, FL. Formerly a general restaurant manager. Likes to learn new things</div>
+                <div className="description">👨‍🍳 German Gamboa is a fullstack developer from Orlando, FL. Formerly a general restaurant manager. Expert waffler at meetup talks</div>
               </div>
             </div>
           </div>
