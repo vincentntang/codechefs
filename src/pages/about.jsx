71c1fabb54e0a166e807a7790b1b084a0b42a157
vincenttang <10290348+vincentntang@users.noreply.github.com>
2020-10-08 00:22:14 -0400

    update social media icons and base styles

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,7 +4,9 @@ import Layout from "../layout";
 import config from "../../data/SiteConfig";
 import vincentImg from "../../static/logos/vincentntang.jpeg";
 import germanImg from "../../static/logos/germangamboa.jpeg";
-
+import socialTwitter from "../assets/social-twitter.svg";
+import socialGlobe from "../assets/social-globe.svg";
+import socialGithub from "../assets/social-github-lightblue.svg";
 
 const AboutPage = () => {
   return (
@@ -21,17 +23,40 @@ const AboutPage = () => {
               <div className="header">
               </div> 
               <div className="img-wrapper">
-                <img src={vincentImg} alt="vincent tang"/>
+                <img className="img-portait" src={vincentImg} alt="vincent tang"/>
               </div>
               <div className="desc-wrapper">
+              <div className="personal-links">
+                  <a href="https://twitter.com/vincentntang">
+                    <img src={socialTwitter} alt="social twitter"/>
+                  </a>
+                  <a href="https://github.com/vincentntang">
+                    <img src={socialGithub} alt="social github"/>
+                  </a>
+                  <a href="https://vincentntang.com">
+                    <img src={socialGlobe} alt="social globe"/>
+                  </a>
+                </div>
                 <div className="description">👨‍🍳 Vincent Tang is a fullstack developer from Orlando, FL. Formerly a kitchen designer. Loves the outdoors and trying new things</div>
+               
               </div>
             </div>
             <div className="cc-padding cc-portrait">
               <div className="img-wrapper">
-                <img src={germanImg} alt="german gamboa"/>
+                <img className="img-portait" src={germanImg} alt="german gamboa"/>
               </div>
               <div className="desc-wrapper">
+                <div className="personal-links">
+                  <a href="https://twitter.com/germangamgon">
+                    <img src={socialTwitter} alt="social twitter"/>
+                  </a>
+                  <a href="https://github.com/germangamboa95">
+                    <img src={socialGithub} alt="social github"/>
+                  </a>
+                  <a href="https://www.linkedin.com/in/german-gamboa-gonzalez-a3863197/">
+                    <img src={socialGlobe} alt="social globe"/>
+                  </a>
+                </div>
                 <div className="description">👨‍🍳 German Gamboa is a fullstack developer from Orlando, FL. Formerly a general restaurant manager. Likes to learn new things</div>
               </div>
             </div>
