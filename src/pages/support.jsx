0ec226ea187ff04c68aa66f2129a18c616abdd37
vincenttang <10290348+vincentntang@users.noreply.github.com>
2020-11-16 17:54:44 -0500

    update support page

--- src/pages/support.jsx
+++ src/pages/support.jsx
@@ -10,12 +10,19 @@ const AboutPage = () => {
         <Helmet title={`About | ${config.siteTitle}`} />
         <div className="cc-card">
           <div className="cc-padding cc-support">
-            <h3 className="color-offgray mt-0">Thanks for dining with us!</h3>
-            <p>If you'd like to support us, follow us on <a className="newsletter-link" href="https://twitter.com/codechefsdev">twitter! </a></p>
-            <p>We're looking to air an episode a week</p>
-            <p>Subscribe to our <a className="newsletter-link" href="https://tinyletter.com/vincentntang">newsletter</a> for more Javascript related content!</p>
+            <h3 className="color-offgray mt-0">Thanks for tuning in!</h3>
+            <p>Follow us on <a className="newsletter-link text-21" href="https://twitter.com/codechefsdev">twitter </a>and our <a className="newsletter-link text-21" href="https://tinyletter.com/vincentntang">newsletter</a> for updates on new episodes!</p>
+            <p>We're looking to air an episode a week!</p>
+            <p>Also don't forget to follow us on your favorite podcast platform!</p>
+            <ul>
+              <li><a target="_blank" rel="noreferrer" href="https://podcasts.apple.com/us/podcast/code-chefs-hungry-web-developer-podcast/id1536223859">Apple Podcasts</a></li>
+              <li><a target="_blank" rel="noreferrer" href="https://podcasts.google.com/feed/aHR0cHM6Ly93d3cuY29kZWNoZWZzLmRldi9yc3MueG1s?sa=X&ved=0CBQQ27cFahcKEwjg4Obtj7_sAhUAAAAAHQAAAAAQAQ">Google Podcasts</a></li>
+              <li><a target="_blank" rel="noreferrer" href="https://open.spotify.com/show/6DxHVg6CAOzwHnLgQClrs7">Spotify</a></li>
+              <li><a target="_blank" rel="noreferrer" href="https://pca.st/wtu4690d">Pocketcasts</a></li>
+              <li><a target="_blank" rel="noreferrer" href="https://overcast.fm/p2403384-t859cb">Overcasts</a></li>
+            </ul>
             <hr/>
-            <p>P.S. like us on your favorite podcast platform!</p>
+            <p>If you'd like to sponsor an episode, please send an email to <a href="mailto:codechefshq@gmail.com">codechefshq@gmail.com</a></p>
           </div>
         </div>
       </div>
