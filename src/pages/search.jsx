fb833b2405cca34968f0fc89cf5c2ab2899bc666
vincenttang <10290348+vincentntang@users.noreply.github.com>
2020-11-25 08:08:51 -0500

    temporary search and sponsor page

---
+++ src/pages/search.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Helmet from "react-helmet";
+import Layout from "../layout";
+import config from "../../data/SiteConfig";
+import PodcastLinks from "../components/PodcastLinks";
+
+const SearchPage = () => {
+  return (
+    <div>Yo hello</div>
+  )
+}
+
+export default SearchPage;
---
+++ src/pages/sponsor.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Helmet from "react-helmet";
+import Layout from "../layout";
+import config from "../../data/SiteConfig";
+import PodcastLinks from "../components/PodcastLinks";
+
+const SponsorPage = () => {
+  return (
+    <div>Yo hello</div>
+  )
+}
+
+export default SponsorPage;
